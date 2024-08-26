1ee09098e613eaf3163152556d57453e4911838f
jiyeonUning <yeon14735@gmail.com>
2024-08-26 16:03:01 +0900

    총알을 쏘는 대포 구현
    오브젝트 생성&파괴 함수와 프리팹에서 알게 된 함수를 사용하여, 총알의 생성과 파괴를 구현할 수 있었다.

---
+++ Assets/Unity240826/Bullet.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] float speed;
+
+    void Start()
+    {
+        Destroy(gameObject, 5f);
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime, Space.Self);
+    }
+
+    public void SetSpeed(float speed) // 값을 수정할 수 있도록 생성자를 생성
+    { this.speed = speed; }
+}
