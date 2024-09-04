151f061d02cd68bcad76ad2c701cfefae89aa174
jiyeonUning <yeon14735@gmail.com>
2024-09-04 16:52:55 +0900

    유니티 : 시네마신 카메라 종류

--- Assets/240829 ~240904/Unity240904/LayerMask/Extension.cs
+++ Assets/240829 ~240904/Unity240904/LayerMask/Extension.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 
 public static class Extension
 {
+    // 확장메서드로 기능을 추가 (레이어마스크 사용 시, 해당 함수를 추가함)
+
     // 레이어를 체크시키는 기능
     public static void Check(this ref LayerMask layerMask, int layer)
     {
         layerMask |= 1 << layer;
     }
 
-    // 레이어를 체크 해제 시키는 기능
+    // 레이어를 체크 해제 시키는 기능 
     public static void UnCheck(this ref LayerMask layerMask, int layer)
     {
         layerMask &= ~(1 << layer);
